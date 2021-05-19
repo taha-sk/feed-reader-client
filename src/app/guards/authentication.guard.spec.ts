fcdaab1bca135e52119493175b2f14653a881d68
Taha Sukru Karabacakoglu <supersawer@gmail.com>
2021-05-19 18:00:59 +0300

    Adding guard tests

--- src/app/guards/authentication.guard.spec.ts
+++ src/app/guards/authentication.guard.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DateTime } from 'luxon';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 import { AuthenticationGuard } from './authentication.guard';
 
 describe('AuthenticationGuard', () => {
   let guard: AuthenticationGuard;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes(
+          [{path: 'login', component: SearchBarComponent}]
+        )
+      ]
+    });
     guard = TestBed.inject(AuthenticationGuard);
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
+
+  it('should not be authenticated', () => {
+    localStorage.removeItem('token_exp_date');
+    expect(guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot)).toEqual(router.parseUrl('/login'));    
+  });
+
+  it('should be authenticated', () => {
+    const tokenExpDate = DateTime.now().plus(1000).toISO();
+    localStorage.setItem('token_exp_date', tokenExpDate);
+    expect(guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot)).toEqual(true);    
+  });
+
 });
--- src/app/guards/authorization.guard.spec.ts
+++ src/app/guards/authorization.guard.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DateTime } from 'luxon';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 import { AuthorizationGuard } from './authorization.guard';
 
 describe('AuthorizationGuard', () => {
   let guard: AuthorizationGuard;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes(
+          [{path: '', component: SearchBarComponent}, {path: 'login', component: SearchBarComponent}]
+        )
+      ]
+    });
     guard = TestBed.inject(AuthorizationGuard);
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
+
+  it('should not be authenticated', () => {
+    localStorage.removeItem('token_exp_date');
+    expect(guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot)).toEqual(router.parseUrl('/login'));    
+  });
+
+  it('should not be admin', () => {
+    const tokenExpDate = DateTime.now().plus(1000).toISO();
+    localStorage.setItem('token_exp_date', tokenExpDate);
+    localStorage.removeItem('admin');
+    expect(guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot)).toEqual(router.parseUrl('/'));    
+  });
+
+  it('should be admin', () => {
+    const tokenExpDate = DateTime.now().plus(1000).toISO();
+    localStorage.setItem('token_exp_date', tokenExpDate);
+    localStorage.setItem('admin', "true");
+    expect(guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot)).toEqual(true);    
+  });
+
 });
