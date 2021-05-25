7ae0e12f568fea36c80f2232f7abf7c10f22d53f
Taha Sukru Karabacakoglu <supersawer@gmail.com>
2021-05-25 12:35:43 +0300

    Adding e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,93 @@
-import { browser, logging } from 'protractor';
-import { AppPage } from './app.po';
+import { browser, logging, protractor } from 'protractor';
+import { FeedReaderPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  let page: FeedReaderPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new FeedReaderPage();
   });
 
-  it('should display welcome message', async () => {
+  it('should display login form', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('feed-reader-client app is running!');
+    expect(await page.getHeaderOneText()).toEqual('Please sign in');
+  });
+
+  it('should login and show main page & navigation links', async () => {
+    await page.navigateTo();
+    await page.login("admin", "admin");
+    //check title
+    expect(await page.getAppTitle()).toEqual('Feed Reader');
+    const navLinks = await page.getNavLinks();
+    //check nav links
+    expect(await navLinks[0].getText()).toEqual('Manage Widgets');
+    expect(await navLinks[1].getText()).toEqual('Log Out');
+  });
+
+  it('should open manage widgets with manage widgets link', async () => {
+    await page.navigateTo();
+    const navLinks = await page.getNavLinks();
+    expect(await navLinks[0].getText()).toEqual('Manage Widgets');
+    await navLinks[0].click();
+    expect(await page.getHeaderTwoText()).toEqual('Manage Widgets');
+  });
+
+  it('should open manage widgets and add widget', async () => {
+    await page.navigateTo();
+    const navLinks = await page.getNavLinks();
+    expect(await navLinks[0].getText()).toEqual('Manage Widgets');
+    await navLinks[0].click();
+    const addWidgetBtn = await page.getElementById('addWidgetBtn');
+    expect(await addWidgetBtn[0].getText()).toEqual("Add Widget");
+    await addWidgetBtn[0].click();
+    //wait for animation
+    let EC = protractor.ExpectedConditions;
+    await browser.wait(EC.stalenessOf(addWidgetBtn[0]), 1000);
+    //make sure that you're on the page
+    expect(await page.getHeaderThreeText()).toEqual('Add Widget');
+    //select quote
+    await page.selectType("Quote");
+    const widgetTitleInpt = await page.getElementById('widgetTitleInpt');
+    await widgetTitleInpt[0].clear();
+    await widgetTitleInpt[0].sendKeys("Test");
+    const submit = await page.getSubmit();
+    await submit[0].click();
+    //return
+    await browser.wait(EC.stalenessOf(submit[0]), 1000);
+    expect(await page.getHeaderThreeText()).toEqual('Widgets');
+  });
+
+  it('should open manage widgets and delete widget', async () => {
+    await page.navigateTo();
+    const navLinks = await page.getNavLinks();
+    expect(await navLinks[0].getText()).toEqual('Manage Widgets');
+    await navLinks[0].click();
+    const rowWidgetTitles = await page.getElementById('rowWidgetTitle');
+    const deleteWidgetBtn = await page.getElementById('deleteWidgetBtn');
+    //number of titles should equal to number of delete buttons
+    expect(rowWidgetTitles.length).toEqual(deleteWidgetBtn.length);
+    //last one should be the test
+    const lastIndex = rowWidgetTitles.length - 1;
+    expect(await rowWidgetTitles[lastIndex].getText()).toEqual("Test");
+    //Then click delete button
+    await deleteWidgetBtn[lastIndex].click();
+    //wait for animation
+    let EC = protractor.ExpectedConditions;
+    await browser.wait(EC.stalenessOf(deleteWidgetBtn[lastIndex]), 1000);
+    const deleteSubmit = await page.getElementById('deleteWidgetBtn');
+    await deleteSubmit[0].click();
+    //return
+    await browser.wait(EC.stalenessOf(deleteSubmit[0]), 1000);
+    expect(await page.getHeaderThreeText()).toEqual('Widgets');
+  });
+
+  it('should logout', async () => {
+    await page.navigateTo();
+    const navLinks = await page.getNavLinks();
+    expect(await navLinks[1].getText()).toEqual('Log Out');
+    await navLinks[1].click();
+    //return to login page
+    expect(await page.getHeaderOneText()).toEqual('Please sign in');
   });
 
   afterEach(async () => {
@@ -20,4 +97,5 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry));
   });
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,48 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
-export class AppPage {
+export class FeedReaderPage {
   async navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl);
   }
 
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+  async getHeaderOneText(): Promise<string> {
+    return element(by.css('h1')).getText();
   }
+
+  async getHeaderTwoText(): Promise<string> {
+    return element(by.css('h2')).getText();
+  }
+
+  async getHeaderThreeText(): Promise<string> {
+    return element(by.css('h3')).getText();
+  }
+
+  async login(user: string, password: string): Promise<void> {
+    await element(by.id('inputUsername')).sendKeys(user);
+    await element(by.id('inputPassword')).sendKeys(password);
+    const submit = await this.getSubmit();
+    await submit[0].click();
+  }
+
+  async getAppTitle(): Promise<string> {
+    return element(by.css('nav > a')).getText();
+  }
+
+  async getNavLinks(): Promise<ElementFinder[]> {
+    return element.all(by.css('a[ngbNavLink]'));
+  }
+
+  async getElementById(id: string): Promise<ElementFinder[]> {
+    return element.all(by.id(id));
+    
+  }
+
+  async selectType(type: string): Promise<void> {
+    await element(by.cssContainingText('option', type)).click();
+  }
+
+  async getSubmit(): Promise<ElementFinder[]> {
+    return element.all(by.css('[type=submit]'));
+  }
+
 }
