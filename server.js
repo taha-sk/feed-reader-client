3705efd53fcdceee1711232dec73389e43ffc7bd
Taha Sukru Karabacakoglu <supersawer@gmail.com>
2026-01-03 21:23:41 +0300

    Dockerize application and update read me

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/feed-reader-client'));
+app.use(express.static('./dist/feed-reader-client/browser'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/feed-reader-client' }
+  res.sendFile('index.html', { root: 'dist/feed-reader-client/browser' }
   );
 });
 
